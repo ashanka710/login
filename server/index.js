624c494d4463d95e93029d22a44a4f082a65bc2c
ashanka710 <ashankad343@gmail.com>
2024-11-26 18:01:48 +0530

    o

--- server/index.js
+++ server/index.js
@@ -126,7 +126,7 @@ app.use(express.static(buildPath));
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
