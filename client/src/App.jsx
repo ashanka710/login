6798f1fe296a5c7df04fb9149cc4074a1e320a31
ashanka710 <ashankad343@gmail.com>
2024-11-27 10:42:47 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Register from './Register';  // Import the Register component
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// Import the Register component
 import Register from './components/register';
 // import Login from './components/login';
+
 const App = () => {
-    return ( <div >
-         <Router>
- <div className="App">
-        {/* <h1>Welcome to the Student Registration System</h1> */}
-
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* You can add more routes here for other components */}
-        </Routes>
-      </div>
-      </Router>
+    return (
+        <div>
+            <Router>
+                <div className="App">
+                    {/* Define Routes */}
+                    <Routes>
+                        {/* Redirect root path (/) to /register */}
+                        <Route path="/" element={<Navigate to="/register" replace />} />
+                        {/* Register route */}
+                        <Route path="/register" element={<Register />} />
+                        {/* Add more routes here as needed */}
+                        {/* <Route path="/login" element={<Login />} /> */}
+                    </Routes>
+                </div>
+            </Router>
         </div>
-    )
-}
+    );
+};
 
-export default App
+export default App;
